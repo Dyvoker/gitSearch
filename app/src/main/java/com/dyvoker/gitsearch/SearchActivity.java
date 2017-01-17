857aeac65962346dbeae51e94424fb97c13c8729
dyvoker <dyvoker@gmail.com>
2017-01-18 00:09:00 +0700

    Add Search Activity and design it.

--- app/src/main/java/com/dyvoker/gitsearch/SearchActivity.java
+++ app/src/main/java/com/dyvoker/gitsearch/SearchActivity.java
@@ -2,6 +2,8 @@ package com.dyvoker.gitsearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
